bebde19cf420e2897af41865b2eb1f546ca284d3
Uliana Deshin <uliana.deshin@gmail.com>
2026-05-20 11:41:49 +1000

    extend w/ test

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,3 +6,17 @@ interface Teacher {
     location: string;
     [propName: string]: any;
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+/*
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+*/
